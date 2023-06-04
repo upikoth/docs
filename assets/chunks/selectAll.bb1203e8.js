7b7f964ee4129e0e5f46faf40182ca992ee801b1
upikoth <upikoth@users.noreply.github.com>
2023-06-04 20:56:57 +0000

    deploy: 6be2268bdf83f2ba8fe32d5d5ab2c3728db782c0

---
+++ assets/chunks/selectAll.bb1203e8.js
@@ -0,0 +1,1 @@
+import{Z as t,_ as n,$ as o}from"../projects_starter.md.48c84b28.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{a as s};
