b1be7daad34a105f1fc3725b5ac068c55776494e
upikoth <upikoth@gmail.com>
2024-01-21 00:02:39 +0300

    feat: добавил яндекс метрику

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,7 @@
+import DefaultTheme from 'vitepress/theme'
+import MyLayout from './my-layout.vue'
+
+export default {
+  extends: DefaultTheme,
+  Layout: MyLayout
+}
---
+++ docs/.vitepress/theme/my-layout.vue
@@ -0,0 +1,33 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+
+// Yandex.Metrika counter
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();
+for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(96194575, "init", {
+			clickmap:true,
+			trackLinks:true,
+			accurateTrackBounce:true,
+			webvisor:true
+});
+
+// Yandex.Metrika counter
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+	 <!-- Yandex.Metrika counter -->
+	<noscript>
+		<div>
+			<img src="https://mc.yandex.ru/watch/96194575" style="position:absolute; left:-9999px;" alt="" />
+		</div>
+	</noscript>
+	<!-- /Yandex.Metrika counter -->
+
+  <Layout />
+</template>
