d335c2adb757a14c29734f4e36aca82e9d64e29f
upikoth <upikoth@users.noreply.github.com>
2024-03-31 15:48:23 +0000

    deploy: 11ef17501fe40270cf313d95a9ed1a5a6449b4d6

--- assets/backend_go.md.tSS0bLO2.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o as e,U as t}from"./chunks/framework.mxDuve1e.js";const g=JSON.parse('{"title":"Go","description":"","frontmatter":{},"headers":[],"relativePath":"backend/go.md","filePath":"backend/go.md"}'),i={name:"backend/go.md"},n=t("",3),o=[n];function l(p,h,r,c,d,_){return e(),a("div",null,o)}const u=s(i,[["render",l]]);export{g as __pageData,u as default};
---
+++ assets/ci-cd_nginx.md.-VwDz4PY.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as i,o as a,U as n}from"./chunks/framework._2-LkFIg.js";const o=JSON.parse('{"title":"Nginx","description":"","frontmatter":{},"headers":[],"relativePath":"ci-cd/nginx.md","filePath":"ci-cd/nginx.md"}'),l={name:"ci-cd/nginx.md"},p=n("",15),e=[p];function t(h,k,r,d,g,c){return a(),i("div",null,e)}const y=s(l,[["render",t]]);export{o as __pageData,y as default};
